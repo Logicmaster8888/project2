9af16dbd548ee80c42a1f5bebb6717693f186cba
Crashoverride <logicmaster8888@gmail.com>
2024-03-07 16:47:06 -0500

    Connected

--- server.js
+++ server.js
@@ -22,10 +22,37 @@ mongoose.connect('mongodb://localhost:27017/my_database', { useNewUrlParser: tru
   .catch(err => console.log(err));
 
 // Define routes
-app.use('/api/users', usersRoutes);
+app.get('/', (req, res) => {
+  console.log(`Received a request on the root endpoint`);
+  const message = `Welcome to our server! This is the root endpoint. You can explore different routes such as /cats for cat-related content or /pokemon to see a list of starter Pokémon.`;
+  res.send(message);
+})
+
+app.get('/cats', (req, res) => {
+  console.log(`Jerry is torturing Tom`);
+  res.send(`Jerry is Communicative`);
+})
+
+
+app.get('/pokemon', (req, res) => {
+  res.send(starterPokemon);
+})
+
+app.get('/jammy', (req, res) => {
+  // res.send will send back 1 thing to the front end
+  // console.log(`something`, `something else`);
+  // res.send(berries);
+  res.send(berries.join(', ') + ', YUM!');
+})
+
+app.get('/jammypokemon', (req, res) => {
+  res.send({berries, starterPokemon})
+})
 
 // Define port
 const PORT = process.env.PORT || 5000;
 
 // Start server
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(5000, () => { 
+  console.log(`SocialSync Is Up & Running`);
+})
