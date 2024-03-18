455abbd44f60decd0974b7561e50590ef95e14d4
Crashoverride <logicmaster8888@gmail.com>
2024-03-18 04:48:32 -0400

    Added Controller for Blogs

--- controllers/blog1Controller.js
+++ controllers/blog1Controller.js
@@ -1,40 +1,47 @@
 
-// // Write a description
-// //@desc Get all blogs
-// //@route GET /api/blogs
-// //@access public
+//@desc Get all blogs
+//@route GET /api/blogs
+//@access public // Later on Private when introduce the Auth
 
-// const Createblog = (req, res) => {
-//     res.statis(200).json({ message: "Get all blogs"});
-// };
+const getBlogs = (req, res) => {
+    res.statis(200).json({ message: `Get All Blogs`});
+};
 
-// const updateblog = (req, res) => {
-//     res.status(200).json({ message: "Create blog" });
-// };
+//@desc Create New Blog
+//@route POST /api/blogs
+//@access public
+
+const createBlog = (req, res) => {
+    res.statis(201).json({ message: `Create Blog`});
+};
 
 
-// const updateblogs = (req, res) => {
-//     res.status(200).json({ message: "Get all blogs" });
-// };
+//@desc Get New Blog
+//@route GET /api/blogs/:id
+//@access public
 
-// //@desc Create New blog
-// //@route POST /api/blogs
-// //@access public
+const getBlog = (req, res) => {
+    res.statis(200).json({ message: `Create Blog for ${req.params.id}`});
+};
 
-// const createblog = (req, res) => {
-//     res.status(201).json({ message: "Create blog"});
+//@desc Update New Blog
+//@route UPDATE /api/blogs/:id
+//@access public
+const updateBlog = (req, res) => {
+    res.status(201).json({ message: `Update Blog for ${req.params.id}`});
+};
 
-// };
+//@desc Delete New Blog
+//@route DELETE /api/blogs/:id
+//@access public
+const deleteBlog = (req, res) => {
+    res.status(200).json({ message: `Delete Blog for ${req.params.id}`});
+};
 
-// //@desc Get blog
-// //@route GET /api/blogs/:id
-// //@access public
-
-// const getblog = (req, res) => {
-//     res.status(200).json({ message: `Get blog for ${req.params.id}`});
-// };
-
-// //@desc Update blog
-// //@route PUT /api/blogs/:id
-// //@access public
-
+module.exports = {
+    getBlogs,
+    createBlog,
+    getBlog,
+    updateBlog,
+    deleteBlog,
+};
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,10 +1,16 @@
 
 const express = require("express");
 const router = express.Router();
-// const {getBlog} = require("../")
+const { 
+        getBlogs,
+        createBlog,
+        getBlog,
+        updateBlog,
+        deleteBlog,
+    } = require("../controllers/blog1Controller");
 
-//@desc Delete Blog
-//@AbortController
+// @desc Delete Blog
+// @AbortController
 
 // router.route("/api/blogs", (req, res) => {
 //     res.status(200).json({ Message: "Can I send code through here?"});
