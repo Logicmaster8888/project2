e3f2660baa8ffc74dc2e95dfbe621907e7dc8acb
Crashoverride <logicmaster8888@gmail.com>
2024-03-18 23:09:27 -0400

    user in models

---
+++ models/user.js
@@ -0,0 +1,20 @@
+// const mongoose = require('mongoose');
+
+
+// const userSchema = new mongoose.Schema({
+//     username: { type: String, required: true, unique: true },
+//     password: { type: String, required: true },
+//     // blogs: [{ type: mongoose.Types.ObjectId, ref: 'Blog'}]
+// });
+
+// module.exports = mongoose.model('User', userSchema);
+
+// // CITATION 
+// /***************************************************************************************
+// *    Title: SEIR-Pixel-Pandas/Fruits
+// *    Author: Billie Heidelberg, Justin White
+// *    Date: 3-16/ 3-18/24
+// *    Code version: Latest 3/18
+// *    Availability: https://git.generalassemb.ly/SEIR-Pixel-Pandas/fruits
+// *
+// ***************************************************************************************/
