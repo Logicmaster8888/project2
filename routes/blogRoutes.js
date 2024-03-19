5121c99b9171347fda75d9502e212d7a5d7f1156
Crashoverride <logicmaster8888@gmail.com>
2024-03-19 03:07:33 -0400

    GETTING ONE ID WORKS

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -27,40 +27,46 @@ router.get('/', (req, res) => {
 //     res.render("index");
 // }); // Change from Sender to Render for specific page// Use res.render('index'); to render index.ejs page
 
-// BLOG POST
-router.get('/blogPost', (req, res) => { // changed for app to Router
-    res.render("blogPost");
+// // BLOG POST // SEE ALL
+// router.get('/blogPost/:id', (req, res) => { // changed for app to Router
+//     res.render("blogPost");
+// });
+
+// GETTING ONE ROUTE WORKS! 
+router.get('/blogPost/:id', (req, res) => {
+    res.send(req.params.id)
 });
+
 
 // EDIT BLOG POSTS // Function is going to be inside of the page
-router.get('/editPosts', (req, res) => { // changed for app to Router
-    res.render("editPosts");
-});
+// router.get('/editPosts', (req, res) => { // changed for app to Router
+//     res.render("editPosts");
+// });
 
-// NEW POST
-router.get('/newPost', (req, res) => { // changed for app to Router
-    res.render("newPost");
-});
+// // NEW POST
+// router.get('/newPost', (req, res) => { // changed for app to Router
+//     res.render("newPost");
+// });
 
-// PROFILE
-router.get('/profile', (req, res) => { // changed for app to Router
-    res.render("profile");
-});
+// // PROFILE
+// router.get('/profile', (req, res) => { // changed for app to Router
+//     res.render("profile");
+// });
 
-// EDIT PROFILE
-router.get('/editProfile', (req, res) => { // changed for app to Router
-    res.render("editProfile");
-});
+// // EDIT PROFILE
+// router.get('/editProfile', (req, res) => { // changed for app to Router
+//     res.render("editProfile");
+// });
 
-// 404 ERROR
-router.get('/404', (req, res) => { // changed for app to Router
-    res.render("404");
-});
+// // 404 ERROR
+// router.get('/404', (req, res) => { // changed for app to Router
+//     res.render("404");
+// });
 
-// EDIT LOGIN / LOGOUT  // Function is going to be inside of the page
-router.get('/login', (req, res) => { // changed for app to Router
-    res.render("login");
-});
+// // EDIT LOGIN / LOGOUT  // Function is going to be inside of the page
+// router.get('/login', (req, res) => { // changed for app to Router
+//     res.render("login");
+// });
 
 
 module.exports = router;
