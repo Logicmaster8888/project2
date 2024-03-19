813f8d8df1d3d09cc31ff1d23881b2f78d10677e
Crashoverride <logicmaster8888@gmail.com>
2024-03-18 22:57:32 -0400

    is Authenticated Controller

---
+++ controllers/IsAuthenticated.js
@@ -0,0 +1,19 @@
+// const isAuthenticated = (req, res, next) => {
+//     if (req.session.currentUser) {
+//         return next();
+//     } else {
+//         res.redirect("/sessions/new");
+//     }
+// };
+
+// module.exports = isAuthenticated;
+
+// CITATION 
+/***************************************************************************************
+*    Title: SEIR-Pixel-Pandas/Fruits
+*    Author: Billie Heidelberg, Justin White
+*    Date: 3-16/ 3-18/24
+*    Code version: Latest 3/18
+*    Availability: https://git.generalassemb.ly/SEIR-Pixel-Pandas/fruits
+*
+***************************************************************************************/
