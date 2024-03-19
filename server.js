6230f838736d6de7e7b85088a0a03757b27da7ba
Crashoverride <logicmaster8888@gmail.com>
2024-03-18 23:20:35 -0400

    Citation for Fruits

--- server.js
+++ server.js
@@ -115,3 +115,13 @@ app.get('*', function (req, res) {
 app.listen(PORT, () => { // callback V
     console.log("Social Media Sync IN SYNC!", PORT); // CALL BACK Console.log
 });
+
+    // CITATION 
+/***************************************************************************************
+*    Title: SEIR-Pixel-Pandas/Fruits
+*    Author: Billie Heidelberg, Justin White, Andrew Deitrick
+*    Date: 3-16/ 3-18/24
+*    Code version: Latest 3/18
+*    Availability: https://git.generalassemb.ly/SEIR-Pixel-Pandas/fruits
+*
+***************************************************************************************/
